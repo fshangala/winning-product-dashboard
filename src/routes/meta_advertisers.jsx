7a080ea100d53b33032a121d88565124213d3ab8
fshangala <fshangala@gmail.com>
2024-06-15 16:52:40 +0200

    basic look for meta advertisers

--- src/routes/meta_advertisers.jsx
+++ src/routes/meta_advertisers.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import metaLogo from "../assets/images/meta.png";
+import MetaAdvertisersFilter from "../components/meta_advertisers_filter";
 
-export default function MetaAdvertisers({}) {
+export default function MetaAdvertisers() {
+
+  const applyFilters = function(filters) {
+    
+  }
+
   return (
     <>
     <div className="header-container">
@@ -9,6 +16,38 @@ export default function MetaAdvertisers({}) {
         <h1>Meta Advertisers</h1>
       </div>
       <hr className="divider" />
+    </div>
+    <MetaAdvertisersFilter applyFilters={applyFilters} />
+    <br />
+    <div className="table-container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Page name</th>
+            <th>Countries</th>
+            <th>Website</th>
+            <th>Ads</th>
+            <th>Adsets</th>
+            <th>Avg. Adsets</th>
+            <th>Likes</th>
+            <th>Followers</th>
+            <th>Created</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
     </>
   )
