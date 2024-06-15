988d9bfdec9d4f01614c9d78e82fc4e153b30422
fshangala <fshangala@gmail.com>
2024-06-15 02:34:27 +0000

    Front end design

--- src/routes/tiktok_creative_center.jsx
+++ src/routes/tiktok_creative_center.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function TiktokCreativeCenter() {
   return (
     <>
@@ -21,8 +23,71 @@ export default function TiktokCreativeCenter() {
         </div>
       </div>
       <div className="download-section">
-        <h3>Video information</h3>
-        <hr />
+        <div className="download-card">
+          <h3>Video information</h3>
+          <hr />
+          <div className="output-section">
+            <div className="details">
+              <div className="input-group">
+                <label>Product URL</label>
+                <input type="text" className="input" placeholder="-" readOnly />
+              </div>
+              <div className="about">
+                <h3>About this Ad</h3>
+                <div className="about-container">
+                  <div className="about-item">
+                    <div>Region</div>
+                    <div>-</div>
+                    <div>Objective</div>
+                    <div>-</div>
+                    <div>Landing Page</div>
+                    <div>-</div>
+                  </div>
+                  <div className="about-item">
+                    <div>Industry</div>
+                    <div>-</div>
+                    <div>Brand name</div>
+                    <div>-</div>
+                    <div>Ad caption</div>
+                    <div>-</div>
+                  </div>
+                </div>
+              </div>
+              <div className="performance">
+                <h3>Ad Performance</h3>
+                <div className="performance-container">
+                  <div className="performance-item">
+                    <div>Likes</div>
+                    <div>-</div>
+                  </div>
+                  <div className="performance-item">
+                    <div>Comments</div>
+                    <div>-</div>
+                  </div>
+                  <div className="performance-item">
+                    <div>Shares</div>
+                    <div>-</div>
+                  </div>
+                  <div className="performance-item">
+                    <div>CTR</div>
+                    <div>-</div>
+                  </div>
+                  <div className="performance-item">
+                    <div>Budget</div>
+                    <div>-</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="video-output">
+              <p>Video without watermark</p>
+              <div className="ad-content">
+                <video className="video"></video>
+                <Link className="download-btn" to="" target="_blank" download>Download</Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     </>
