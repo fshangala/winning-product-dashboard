b1e0cd9a076c65c0c40b84c081028d6ae010c7ad
fshangala <fshangala@gmail.com>
2024-06-29 05:43:25 +0200

    header component

--- src/routes/sales_tracker.jsx
+++ src/routes/sales_tracker.jsx
@@ -1,5 +1,13 @@
 export default function SalesTracker() {
   return (
-    <></>
+    <>
+    <div className="header-container">
+      <div className="header">
+        {/* <img src={metaLogo} alt="meta" /> */}
+        <h1>Sales Tracker</h1>
+      </div>
+      <hr className="divider" />
+    </div>
+    </>
   )
 }
