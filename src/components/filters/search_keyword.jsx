86ab4772cb9b65c5a4834f02259570e3d42b5108
fshangala <fshangala@gmail.com>
2024-09-06 09:05:33 +0200

    filters

--- src/components/filters/search_keyword.jsx
+++ src/components/filters/search_keyword.jsx
@@ -1,22 +1,26 @@
 import { useEffect, useRef, useState } from "react"
 
-export default function SearchKeyword({value="All",onChange=function({value}){}}) {
+export default function SearchKeyword({value="",onChange=function({value}){}}) {
   const refElem = useRef(null)
 
+  const searchKeywordData = [
+    {id:"",text:"All"},
+    {id:"adtext",text:"Ad Text"},
+    {id:"landingurl",text:"Landing Url"},
+    {id:"pagename",text:"Page Name"},
+    {id:"productname",text:"Product Name"},
+  ]
+
   useEffect(function(){
-    $(refElem.current).select2()
+    $(refElem.current).select2({
+      data:searchKeywordData,
+    })
   },[])
 
   return (
     <div className="select-wrapper">
       <div className="select-label">Search Keyword In...</div>
-      <select ref={refElem} className="form-input select-box-2 w-select" onChange={function(){onChange({value:e.target.value})}} value={value}>
-        <option value="All" data-select2-id="2">All</option>
-        <option value="adtext">Ad Text</option>
-        <option value="landingurl">Landing Url</option>
-        <option value="pagename">Page Name</option>
-        <option value="productname">Product Name</option>
-      </select>
+      <select ref={refElem} className="form-input select-box-2 w-select" onChange={function(){onChange({value:e.target.value})}} value={value}></select>
     </div>
   )
 }
